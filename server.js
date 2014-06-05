f18d571caf383f8680139468827a7a2142bd93b4
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-06-05 09:24:03 +0700

    remove unuse file

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+/**
+*
+*/
 var express = require('express');
 var app = express();
 var path = require("path");
 var db = require('./config/db');
 var port = process.env.PORT || 8080;
 
+/**
+*
+*/
 app.configure(function () {
     app.use(express.static(path.join(__dirname, '/public')));
     app.use(express.logger('dev'));
